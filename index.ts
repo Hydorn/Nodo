ab88636ea52cfd0344014c694082634d84fba590
Hydorn <ema.villanueva1995@gmail.com>
2022-10-25 10:34:11 -0300

    app.post and product properties edited

--- index.ts
+++ index.ts
@@ -6,31 +6,33 @@ app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 })
+
 type products = {
     id: number;
     name: string;
     brand: string;
+    price: number
 }
 
-const product :Array<products>= [{id:1234,name:"Juan",brand:"ADIDAS"}];
+const product :Array<products>= [{id:1234,name:"Juan",brand:"ADIDAS", price:666}];
 
 app.get("/product",(req,res)=>{
     res.json(product);
 });
 
 app.post("/product", (req, res) => {
-    const { name, brand } = req.body;
+    const { name, brand , price} = req.body;
     try {
-      if (!name && !brand) throw new Error("che pasame el name y marca");
-      product.push({ id: new Date().getTime(), name, brand });
+      if (!name && !brand && !price) throw new Error("Need name brand and price");
+      product.push({ id: new Date().getTime(), name, brand, price });
       res.status(200).json(product);
     } catch (err) {
       return res.status(400).json({ message: err.message });
     }
 });
-
+/*
 app.put("/product", (req, res) => {
-    const { name, brand, id } = req.body;    
+    const { name, brand, id , price} = req.body;    
     const index = product.findIndex((product) => product.id === id);
     if (!index) product[index] = {name, brand, id};
     res.status(200).json(product[index]);
@@ -41,4 +43,5 @@ app.delete("/product",(req,res)=>{
     const index = product.findIndex((product) => product.id === id);
     res.status(200).json(product[index]);
 })
+*/
 app.listen(3000);
