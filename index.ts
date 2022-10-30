b6037563ea97701819a09ecbba1bf36901e02ea9
Hydorn <ema.villanueva1995@gmail.com>
2022-10-30 13:26:35 -0300

    Modified DELETE, UPDATE, routes with id now

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ type products = {
     price: number
 }
 
-const product :Array<products>= [{id:1234,name:"Juan",brand:"ADIDAS", price:666}];
+const product :Array<products>= [{id:1234,name:"Juan",brand:"ADIDAS", price:666},{id:111,name:"Juan",brand:"ADIDAS", price:666},{id:123,name:"Juan",brand:"ADIDAS", price:666}];
 
 app.get("/product",(req,res)=>{
     res.json(product);
@@ -29,22 +29,21 @@ app.post("/product", (req, res) => {
     }
 });
 
-app.put("/product", (req, res) => {
-    const { name, brand, id , price} = req.body;    
+app.put("/product/:id", (req, res) => {
+    const id =  Number(req.params?.id);
+    const { name, brand, price} = req.body;    
     const index = product.findIndex((product) => product.id === id);
     if (!index) product[index] = {name, brand, id, price};
     res.status(200).json(product[index]);
   });
 
-app.delete("/product",(req,res)=>{
-    let deleted:number;
-    const {id} = req.body;
-    const index = product.findIndex((product) => Number(product.id) === Number(id));
-    if(index!=-1){
-      deleted = Number(product.splice(index,1));
+app.delete("/product/:id",(req,res)=>{
+  const id = req.params?.id;
+  const index = product.findIndex((product) => Number(product.id) === Number(id));
+  if(index!=-1){
+      const deleted:products = product.splice(index,1)[0];
+      res.status(200).json(deleted);
     }
-    res.status(200).json(deleted);
-
 })
 
 app.listen(3000);
