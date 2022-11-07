c55c708fb9868f278611a8a8a7e038d690284f0e
Hydorn <ema.villanueva1995@gmail.com>
2022-11-07 16:41:09 -0300

    search and pagination, GET in /products route

--- index.ts
+++ index.ts
@@ -12,16 +12,51 @@ type products = {
     price: number
 }
 
-const product :Array<products>= [{id:1234,name:"Juan",brand:"ADIDAS", price:666},{id:111,name:"Juan",brand:"ADIDAS", price:666},{id:123,name:"Juan",brand:"ADIDAS", price:666}];
+const product :Array<products>= [ {id:1234,name:"Juan",brand:"ADIDAS", price:666},
+                                  {id:111,name:"Juan",brand:"ADIDAS", price:666},
+                                  {id:123,name:"Juan",brand:"ADIDAS", price:666},
+                                  {id:1121,name:"Fernet",brand:"Branca", price:666},
+                                  {id:1511,name:"Mayonesa",brand:"Natura", price:666},
+                                  {id:11221,name:"Aceite",brand:"Cocinero", price:666},
+                                  {id:157911,name:"Cerveza",brand:"Melorto", price:666},
+                                  {id:511,name:"Chocolate",brand:"Milka", price:666},
+                                  {id:55711,name:"Leche",brand:"Sancor", price:666},
+                                  {id:15711,name:"Manteca",brand:"Milkaut", price:666},
+                                  {id:55711,name:"Queso",brand:"La serenisima", price:666},
+                                  {id:55711,name:"TV",brand:"SAMSUNG", price:666}];
 
-app.get("/product",(req,res)=>{
-    res.json(product);
+app.get("/product/:page?",(req,res)=>{
+  let filtered = product;
+  //Search and filter
+  let query = String(req.query.search).toLocaleLowerCase();
+
+  filtered = filtered?.filter(item=>{
+    return (item.name.toLocaleLowerCase().includes(query)||
+        item.brand.toLocaleLowerCase().includes(query));
+  })
+
+  //Pagination
+  let paginatedProducts: Array<products[]> = [];
+
+  for(let i=0; i<product.length; i=i+5){
+    paginatedProducts.push(filtered.slice(i,i+5));
+  }
+
+  //Page 
+  let page = Number(req.params.page);
+
+  //Response if page not specified
+  if(!page) res.json(paginatedProducts[0]);
+  else res.json(paginatedProducts[page])
+
 });
 
 app.post("/product", (req, res) => {
     const { name, brand , price} = req.body;
     try {
-      if (!name && !brand && !price) throw new Error("Need name brand and price");
+      if (!name) throw new Error("Need name");
+      if (!brand) throw new Error("Need brand");
+      if (!price) throw new Error("Need price");
       product.push({ id: new Date().getTime(), name, brand, price });
       res.status(200).json(product);
     } catch (err) {
